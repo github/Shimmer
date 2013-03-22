cccd598e8fe20c016e89c8a7f7948774836f274c
Brendan Forster <me@brendanforster.com>
2013-03-22 13:06:09 -0400

    removing unused using statements and dead code

--- src/SampleUpdatingApp/MainWindow.xaml.cs
+++ src/SampleUpdatingApp/MainWindow.xaml.cs
@@ -1,28 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ReactiveUI;
 using ReactiveUI.Xaml;
 using Shimmer.Client;
 
 namespace SampleUpdatingApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
