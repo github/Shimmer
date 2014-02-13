0969da999b14e0653beaef64978af30452c31a9b
Richard Wang <zhwang@live.com.au>
2014-02-13 17:34:58 +1100

    Friendlier bootstrap fail when Git isn't in PATH

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -5,7 +5,16 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $rootFolder = (Get-Item $scriptPath).Parent.FullName
 $srcFolder = "$rootFolder\src"
 
-git submodule init
+try {
+    git submodule init
+} catch {
+    Write-Host "Git not found!" -ForegroundColor Red
+    Write-Host
+    Write-Host "If you have Git installed you should add at least PATH_TO_GIT\cmd to your path"
+    Write-Host "For most people, this would be C:\Program Files (x86)\Git\cmd"
+    Write-Host
+    throw
+}
 
 $nuget = "$srcFolder\.nuget\nuget.exe"
 
