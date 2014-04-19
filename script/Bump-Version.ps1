c8fad88a51888ec1c13044cd9c18eb96b215f035
Dennis Daume <daume.dennis@gmail.com>
2014-04-20 00:00:58 +0200

    Fix the Bump-Version script
    Previously it would include any occurrence of "Squirrel." in the path, for example "C:\\Squirrel.Windows\src\SampleUpdatingApp"
    
    Also fixed a typo

--- script/Bump-Version.ps1
+++ script/Bump-Version.ps1
@@ -70,7 +70,7 @@ $srcFolder = "$rootFolder\src"
 
 $items = Get-ChildItem -Path "$srcFolder" -Filter "AssemblyInfo.cs" -Recurse
 
-$items = $items | Where-Object {$_.FullName.Contains("Squirrel.") -or $_.FullName.COntains("CreateReleasePackage")}
+$items = $items | Where-Object {$_.FullName.Contains("src\Squirrel.") -or $_.FullName.Contains("src\CreateReleasePackage")}
 
 $currentVersion = [System.Version](Read-VersionAssemblyInfo $items[0].FullName)
 
